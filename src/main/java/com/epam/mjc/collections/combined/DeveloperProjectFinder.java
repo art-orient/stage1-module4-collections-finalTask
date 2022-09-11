446615dea983e397cd34d784bdfacd913273e059
Aliaksandr Artsikhovich <art-orient@tut.by>
2022-09-11 23:12:53 +0300

    done

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,27 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> list = new ArrayList<>();
+        for (Map.Entry<String, Set<String>> entry : projects.entrySet()) {
+            if (entry.getValue().contains(developer)) {
+                list.add(entry.getKey());
+            }
+        }
+        list.sort(new LengthComparator());
+        return list;
+    }
+
+    static class LengthComparator implements Comparator<String> {
+        @Override
+        public int compare(String o1, String o2) {
+            int result = o2.length() - o1.length();
+            if (result == 0) {
+                result = o2.compareTo(o1);
+            }
+            return result;
+        }
     }
 }
